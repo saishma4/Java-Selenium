3c3e87b8abc539c8057c6c40738ffc68330a2d8b
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-14 00:09:18 +0530

    java loops commit

---
+++ src/loopsDemo/Forloop.java
@@ -0,0 +1,15 @@
+package loopsDemo;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("for loop started");
+		for(int iterator=1;iterator<=20;iterator++) {
+			System.out.println(iterator);
+			
+		}
+		System.out.println("for loop ended");
+	}
+
+}
---
+++ src/loopsDemo/Whileloop.java
@@ -0,0 +1,21 @@
+package loopsDemo;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 50;
+		while(count<60) {
+			System.out.println(count);
+			++count;
+		}
+		int id=34;
+		do {
+			System.out.println("Id: "+id);
+			id++;
+		}while(id<=40);
+		
+
+	}
+
+}
